9da2a0473bd8d75a24eea84fa4298d19811f5046
guoxk <gxk1744413631@163.com>
2025-12-17 23:22:27 +0800

    chore: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/http',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/typescript-runtime',
+    '@nuxt/typescript-build'
+  ]
+})
